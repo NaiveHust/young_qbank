47a9093ecac3467e75ce44ac7fc1ecf3db481f98
zhangaoyun <hiaoyun@qq.com>
2021-07-04 17:13:13 +0800

    docx OK

--- src/office/paper-generator.js
+++ src/office/paper-generator.js
@@ -14,6 +14,7 @@ var PaperCreator = /** @class */ (function () {
     }
     PaperCreator.prototype.create = function (jsonStr) {
         var number = 0;
+        var numberAnswer = 0;
         var paperObj = JSON.parse(jsonStr);
         var single = paperObj.Single;
         var singleTopic = single.topic;
@@ -32,6 +33,17 @@ var PaperCreator = /** @class */ (function () {
                             text: paperObj.Info.name,
                             heading: docx_1.HeadingLevel.TITLE,
                             alignment: docx_1.AlignmentType.CENTER
+                        }),
+                        new docx_1.Paragraph({
+                            text: "满分：" + paperObj.Info.score + "考试时长：" + paperObj.Info.duration,
+                            alignment: docx_1.AlignmentType.CENTER
+                        }),
+                        new docx_1.Paragraph({
+                            text: "姓名：________" + "班级：__________" + "学号：__________",
+                            alignment: docx_1.AlignmentType.CENTER
+                        }),
+                        new docx_1.Paragraph({
+                            text: ""
                         }),
                         new docx_1.Paragraph({
                             text: "单选题",
@@ -196,6 +208,186 @@ var PaperCreator = /** @class */ (function () {
                     })
                         .reduce(function (prev, curr) { return prev.concat(curr); }, []))
                 },
+                // 答案部分
+                {
+                    children: __spreadArrays([
+                        new docx_1.Paragraph({
+                            text: paperObj.Info.name + " 答案及解析",
+                            heading: docx_1.HeadingLevel.TITLE,
+                            alignment: docx_1.AlignmentType.CENTER
+                        }),
+                        new docx_1.Paragraph({
+                            text: "单选题",
+                            heading: docx_1.HeadingLevel.HEADING_1
+                        })
+                    ], singleTopic
+                        .map(function (topic) {
+                        var arr = [];
+                        arr.push(
+                        // 小题空一行
+                        new docx_1.Paragraph({
+                            text: " "
+                        }));
+                        arr.push(new docx_1.Paragraph({
+                            text: "\u7B2C " + ++numberAnswer + " \u9898 \u7B54\u6848\uFF1A" + topic.answer + "解析：" + topic.explain,
+                            heading: docx_1.HeadingLevel.HEADING_2
+                        }));
+                        // 每个单选题的选项
+                        arr.push.apply(arr, topic.choice
+                            .map(function (c) {
+                            var array = [];
+                            array.push(new docx_1.Paragraph({
+                                text: c.name + ". " + c.content
+                            }));
+                            return array;
+                        })
+                            .reduce(function (prev, curr) { return prev.concat(curr); }, []));
+                        return arr;
+                    })
+                        .reduce(function (prev, curr) { return prev.concat(curr); }, []), [
+                        // 大题空两行
+                        new docx_1.Paragraph({
+                            text: " "
+                        }),
+                        new docx_1.Paragraph({
+                            text: " "
+                        }),
+                        new docx_1.Paragraph({
+                            text: "多选题",
+                            heading: docx_1.HeadingLevel.HEADING_1
+                        })
+                    ], multipleTopic
+                        .map(function (topic) {
+                        var arr = [];
+                        arr.push(
+                        // 小题空一行
+                        new docx_1.Paragraph({
+                            text: " "
+                        }));
+                        arr.push(new docx_1.Paragraph({
+                            text: "\u7B2C " + ++numberAnswer + " \u9898 " + topic.question + "\n",
+                            heading: docx_1.HeadingLevel.HEADING_2
+                        }));
+                        // 每个多选题的选项
+                        arr.push.apply(arr, topic.answer
+                            .map(function (a) {
+                            var array = [];
+                            array.push(new docx_1.Paragraph({
+                                text: a + ""
+                            }));
+                            return array;
+                        })
+                            .reduce(function (prev, curr) { return prev.concat(curr); }, []));
+                        arr.push(new docx_1.Paragraph({
+                            text: "解析：" + topic.explain
+                        }));
+                        return arr;
+                    })
+                        .reduce(function (prev, curr) { return prev.concat(curr); }, []), [
+                        // 大题空两行
+                        new docx_1.Paragraph({
+                            text: " "
+                        }),
+                        new docx_1.Paragraph({
+                            text: " "
+                        }),
+                        new docx_1.Paragraph({
+                            text: "判断题",
+                            heading: docx_1.HeadingLevel.HEADING_1
+                        })
+                    ], trueOrFalseTopic
+                        .map(function (topic) {
+                        var arr = [];
+                        arr.push(
+                        // 小题空一行
+                        new docx_1.Paragraph({
+                            text: " "
+                        }));
+                        arr.push(new docx_1.Paragraph({
+                            text: "\u7B2C " + ++numberAnswer + " \u9898 \u7B54\u6848\uFF1A" + topic.answer + "解析：" + topic.explain,
+                            heading: docx_1.HeadingLevel.HEADING_2
+                        }));
+                        return arr;
+                    })
+                        .reduce(function (prev, curr) { return prev.concat(curr); }, []), [
+                        // 大题空两行
+                        new docx_1.Paragraph({
+                            text: " "
+                        }),
+                        new docx_1.Paragraph({
+                            text: " "
+                        }),
+                        new docx_1.Paragraph({
+                            text: "填空题",
+                            heading: docx_1.HeadingLevel.HEADING_1
+                        })
+                    ], fillTopic
+                        .map(function (topic) {
+                        var arr = [];
+                        arr.push(
+                        // 小题空一行
+                        new docx_1.Paragraph({
+                            text: " "
+                        }));
+                        arr.push(new docx_1.Paragraph({
+                            text: "\u7B2C " + ++numberAnswer + " \u9898 ",
+                            heading: docx_1.HeadingLevel.HEADING_2
+                        }));
+                        // 每个填空题的每一空
+                        arr.push.apply(arr, topic.question
+                            .map(function (q) {
+                            var array = [];
+                            array.push(new docx_1.Paragraph({
+                                text: q.answer
+                            }));
+                            return array;
+                        })
+                            .reduce(function (prev, curr) { return prev.concat(curr); }, []));
+                        arr.push(new docx_1.Paragraph({
+                            text: "解析：" + topic.explain
+                        }));
+                        return arr;
+                    }).reduce(function (prev, curr) { return prev.concat(curr); }, []), [
+                        // 大题空两行
+                        new docx_1.Paragraph({
+                            text: " "
+                        }),
+                        new docx_1.Paragraph({
+                            text: " "
+                        }),
+                        new docx_1.Paragraph({
+                            text: "简答题",
+                            heading: docx_1.HeadingLevel.HEADING_1
+                        })
+                    ], answerTopic
+                        .map(function (topic) {
+                        var arr = [];
+                        arr.push(
+                        // 小题空一行
+                        new docx_1.Paragraph({
+                            text: " "
+                        }));
+                        arr.push(new docx_1.Paragraph({
+                            text: "\u7B2C " + ++numberAnswer + " \u9898 " + topic.question + "\n",
+                            heading: docx_1.HeadingLevel.HEADING_2
+                        }));
+                        // 每个简答题的每一问
+                        arr.push.apply(arr, topic.subQ
+                            .map(function (s) {
+                            var array = [];
+                            array.push(new docx_1.Paragraph({
+                                text: "答案" + s.answer
+                            }));
+                            return array;
+                        })
+                            .reduce(function (prev, curr) { return prev.concat(curr); }, []));
+                        arr.push(new docx_1.Paragraph({
+                            text: "解析：" + topic.explain
+                        }));
+                        return arr;
+                    })
+                        .reduce(function (prev, curr) { return prev.concat(curr); }, []))
+                }
             ]
         });
         for (var i = 0; i < paperObj.Single.topic.length; i++) {
--- src/office/paper-generator.ts
+++ src/office/paper-generator.ts
@@ -3,6 +3,7 @@ import {AlignmentType, Document, HeadingLevel, Paragraph, TabStopPosition, TabSt
 export class PaperCreator {
   public create(jsonStr): Document {
     let number = 0;
+    let numberAnswer = 0;
     let paperObj = JSON.parse(jsonStr)
     let single = paperObj.Single
     let singleTopic = single.topic
@@ -25,6 +26,17 @@ export class PaperCreator {
               alignment: AlignmentType.CENTER,
             }),
             new Paragraph({
+              text: "满分：" + paperObj.Info.score + "考试时长：" + paperObj.Info.duration,
+              alignment: AlignmentType.CENTER,
+            }),
+            new Paragraph({
+              text: "姓名：________" + "班级：__________" + "学号：__________",
+              alignment: AlignmentType.CENTER,
+            }),
+            new Paragraph({
+              text: ""
+            }),
+            new Paragraph({
               text: "单选题",
               heading: HeadingLevel.HEADING_1,
             }),
@@ -39,7 +51,7 @@ export class PaperCreator {
                 );
                 arr.push(
                   new Paragraph({
-                    text: `第 ${++number} 题 `+ topic.question + "\n",
+                    text: `第 ${++number} 题 ` + topic.question + "\n",
                     heading: HeadingLevel.HEADING_2,
                   })
                 )
@@ -84,7 +96,7 @@ export class PaperCreator {
                 )
                 arr.push(
                   new Paragraph({
-                    text: `第 ${++number} 题 `+ topic.question + "\n",
+                    text: `第 ${++number} 题 ` + topic.question + "\n",
                     heading: HeadingLevel.HEADING_2,
                   })
                 )
@@ -129,7 +141,7 @@ export class PaperCreator {
                 )
                 arr.push(
                   new Paragraph({
-                    text: `第 ${++number} 题 `+ topic.question + "\n",
+                    text: `第 ${++number} 题 ` + topic.question + "\n",
                     heading: HeadingLevel.HEADING_2,
                   })
                 )
@@ -204,7 +216,7 @@ export class PaperCreator {
                 )
                 arr.push(
                   new Paragraph({
-                    text: `第 ${++number} 题 `+ topic.question + "\n",
+                    text: `第 ${++number} 题 ` + topic.question + "\n",
                     heading: HeadingLevel.HEADING_2,
                   })
                 )
@@ -229,6 +241,232 @@ export class PaperCreator {
               .reduce((prev, curr) => prev.concat(curr), []),
           ]
         },
+        // 答案部分
+        {
+          children: [
+            new Paragraph({
+              text: paperObj.Info.name + " 答案及解析",
+              heading: HeadingLevel.TITLE,
+              alignment: AlignmentType.CENTER,
+            }),
+            new Paragraph({
+              text: "单选题",
+              heading: HeadingLevel.HEADING_1,
+            }),
+            ...singleTopic
+              .map(topic => {
+                const arr: Paragraph[] = [];
+                arr.push(
+                  // 小题空一行
+                  new Paragraph({
+                    text: " "
+                  }),
+                );
+                arr.push(
+                  new Paragraph({
+                    text: `第 ${++numberAnswer} 题 答案：` + topic.answer + "解析：" + topic.explain,
+                    heading: HeadingLevel.HEADING_2,
+                  })
+                )
+                // 每个单选题的选项
+                arr.push(
+                  ...topic.choice
+                    .map(c => {
+                      let array: Paragraph[] = []
+                      array.push(
+                        new Paragraph({
+                          text: c.name + ". " + c.content,
+                        })
+                      )
+
+                      return array
+                    })
+                    .reduce((prev, curr) => prev.concat(curr), []),
+                )
+
+                return arr;
+              })
+              .reduce((prev, curr) => prev.concat(curr), []),
+            // 大题空两行
+            new Paragraph({
+              text: " "
+            }),
+            new Paragraph({
+              text: " "
+            }),
+            new Paragraph({
+              text: "多选题",
+              heading: HeadingLevel.HEADING_1
+            }),
+            ...multipleTopic
+              .map(topic => {
+                const arr: Paragraph[] = [];
+                arr.push(
+                  // 小题空一行
+                  new Paragraph({
+                    text: " "
+                  }),
+                )
+                arr.push(
+                  new Paragraph({
+                    text: `第 ${++numberAnswer} 题 ` + topic.question + "\n",
+                    heading: HeadingLevel.HEADING_2,
+                  })
+                )
+                // 每个多选题的选项
+                arr.push(
+                  ...topic.answer
+                    .map(a => {
+                      let array: Paragraph[] = []
+                      array.push(
+                        new Paragraph({
+                          text: a + ""
+                        })
+                      )
+
+                      return array
+                    })
+                    .reduce((prev, curr) => prev.concat(curr), []),
+                )
+                arr.push(
+                  new Paragraph({
+                    text: "解析：" + topic.explain
+                  })
+                )
+                return arr;
+              })
+              .reduce((prev, curr) => prev.concat(curr), []),
+            // 大题空两行
+            new Paragraph({
+              text: " "
+            }),
+            new Paragraph({
+              text: " "
+            }),
+            new Paragraph({
+              text: "判断题",
+              heading: HeadingLevel.HEADING_1
+            }),
+            ...trueOrFalseTopic
+              .map(topic => {
+                const arr: Paragraph[] = [];
+                arr.push(
+                  // 小题空一行
+                  new Paragraph({
+                    text: " "
+                  }),
+                )
+                arr.push(
+                  new Paragraph({
+                    text: `第 ${++numberAnswer} 题 答案：` + topic.answer + "解析：" + topic.explain,
+                    heading: HeadingLevel.HEADING_2,
+                  })
+                )
+
+                return arr
+              })
+              .reduce((prev, curr) => prev.concat(curr), []),
+            // 大题空两行
+            new Paragraph({
+              text: " "
+            }),
+            new Paragraph({
+              text: " "
+            }),
+            new Paragraph({
+              text: "填空题",
+              heading: HeadingLevel.HEADING_1,
+            }),
+            ...fillTopic
+              .map(topic => {
+
+                const arr: Paragraph[] = [];
+                arr.push(
+                  // 小题空一行
+                  new Paragraph({
+                    text: " "
+                  }),
+                )
+                arr.push(
+                  new Paragraph({
+                    text: `第 ${++numberAnswer} 题 `,
+                    heading: HeadingLevel.HEADING_2,
+                  })
+                )
+                // 每个填空题的每一空
+                arr.push(
+                  ...topic.question
+                    .map(q => {
+                      let array: Paragraph[] = []
+                      array.push(
+                        new Paragraph({
+                          text: q.answer
+                        })
+                      )
+
+                      return array
+                    })
+                    .reduce((prev, curr) => prev.concat(curr), []),
+                )
+                arr.push(
+                  new Paragraph({
+                    text: "解析：" + topic.explain
+                  })
+                )
+                return arr;
+              }).reduce((prev, curr) => prev.concat(curr), []),
+            // 大题空两行
+            new Paragraph({
+              text: " "
+            }),
+            new Paragraph({
+              text: " "
+            }),
+            new Paragraph({
+              text: "简答题",
+              heading: HeadingLevel.HEADING_1
+            }),
+            ...answerTopic
+              .map(topic => {
+                const arr: Paragraph[] = [];
+                arr.push(
+                  // 小题空一行
+                  new Paragraph({
+                    text: " "
+                  }),
+                )
+                arr.push(
+                  new Paragraph({
+                    text: `第 ${++numberAnswer} 题 `,
+                    heading: HeadingLevel.HEADING_2,
+                  })
+                )
+                // 每个简答题的每一问
+                arr.push(
+                  ...topic.subQ
+                    .map(s => {
+                      let array: Paragraph[] = []
+                      array.push(
+                        new Paragraph({
+                          text: "答案" + s.answer,
+                        })
+                      )
+
+                      return array
+                    })
+                    .reduce((prev, curr) => prev.concat(curr), []),
+                )
+                arr.push(
+                  new Paragraph({
+                    text: "解析：" + topic.explain
+                  })
+                )
+
+                return arr;
+              })
+              .reduce((prev, curr) => prev.concat(curr), []),
+          ]
+        }
       ]
     })
 
