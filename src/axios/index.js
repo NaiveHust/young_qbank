3f7d8061eeae4f91aec39852b80af41af4a4040c
hustxhy <1305642473@qq.com>
2021-07-11 14:37:41 +0800

    连接部署后的后端

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: 肖环宇
  * @Date: 2021-07-06 09:58:34
- * @LastEditTime: 2021-07-10 19:08:53
+ * @LastEditTime: 2021-07-11 14:11:31
  * @LastEditors: 肖环宇
  * @Description: 
  */
@@ -9,17 +9,17 @@ import axios from "axios";
 
 
 const config = {
-    baseURL: process.env.NODE_ENV === 'production' ? 'http://localhost:9000/api/v1' : 'http://localhost:9000/api/v1',
+    baseURL: process.env.NODE_ENV === 'production' ? 'http://123.57.215.118:9000/api/v1' : 'http://123.57.215.118:9000/api/v1',
     timeout: 5000
 }
 
 const paperConfig = {
-    baseURL: process.env.NODE_ENV === 'production' ? 'http://localhost:7000/api/v1' : 'http://localhost:7000/api/v1',
+    baseURL: process.env.NODE_ENV === 'production' ? 'http://101.201.151.170:7000/api/v1' : 'http://101.201.151.170:7000/api/v1',
     timeout: 5000
 }
 
 const qsConfig = {
-    baseURL: process.env.NODE_ENV === 'production' ? 'http://localhost:8000/api/v1' : 'http://localhost:8000/api/v1',
+    baseURL: process.env.NODE_ENV === 'production' ? 'http://101.201.151.170:8000/api/v1' : 'http://101.201.151.170:8000/api/v1',
     timeout: 5000
 }
 const instance = axios.create(config);
